4279428eb1b1d8b56a0663b79cd7f8783b2e174d
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2018-08-29 16:32:18 +0200

    add uninstall skript

---
+++ airtame/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿# Airtame uninstall
+
+$ErrorActionPreference = 'Stop';
+
+$toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Import-Module -Name "$($toolsDir)\helpers.ps1"
+
+Remove-FileItem `
+    -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Airtame.lnk" 
