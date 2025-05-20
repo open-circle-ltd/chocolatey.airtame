f228b111a97c13ac87258785939b58ed036407ab
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-05-20 13:51:55 +0200

    Version update to 4.12.0

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -5,9 +5,8 @@ $ErrorActionPreference = 'Stop';
 $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $PackageParameters   = Get-PackageParameters
 
-# https://airtame-app.b-cdn.net/app/latest/win/Airtame-4.12.0-setup.msi
-$urlPackage = "https://airtame-app.b-cdn.net/app/latest/win/Airtame-4.10.0-setup.msi"
-$checksumPackage = "dbcea68ab66655faa15c5aa3c0f3cc4de22b3470b0186a5b24a9f595c34befebd875884d25a0e92a333e79d1389e8eb35534d9dd01de16e85b6af142910b1120"
+$urlPackage = "https://airtame-app.b-cdn.net/app/latest/win/Airtame-4.12.0-setup.msi"
+$checksumPackage = "5adab036471f11e6b16ba25dce329c5cd966d70e8ceb8c9efc98036e8f4b8b547b2a7b8d7c513e4c19f45ccc00b0e2440c87d1216674cddb44a9ec8dd9a04e05"
 $checksumTypePackage = 'SHA512'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
